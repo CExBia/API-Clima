8c2d7cb2f1bd8c16127c96ff139098d659f74614
CExBia <40669666+CExBia@users.noreply.github.com>
2018-07-12 10:18:59 -0300

    Update API-Clima.py

--- API-Clima.py
+++ API-Clima.py
@@ -5,11 +5,11 @@ import requests
 app = Flask(__name__)
 @app.route('/temperatura', methods=['POST'])
 def temperatura():
-    city_name = request.form('q')
-    r = requests.get('https://owm.io/data/2.5/weather?q='+city_name+',uk&appid=18db156f5ef1c6bdad1be1c5072fa282')
+    city_name = request.form['q']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+'&APPID=18db156f5ef1c6bdad1be1c5072fa282')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
-    temp_c = (temp_k - 273.15)
+    temp_c = int(temp_k - 273.15)
     
     return render_template('temperatura.html', temp=temp_c)
 
