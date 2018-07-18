78c59fd5fe1eda34ec8f203ed36a76ff1aa07214
CExBia <40669666+CExBia@users.noreply.github.com>
2018-07-18 09:03:37 -0300

    Update API-Clima.py

--- API-Clima.py
+++ API-Clima.py
@@ -6,23 +6,18 @@ import requests
 import os
 
 app = Flask(__name__, template_folder='templates')
+log = app.logger
+
 @app.route('/temperatura', methods=['POST'])
+
 def temperatura():
-    city_name = request.form['city']
+    req = request.get_json(silent=True, force=True)
+    city_name = req["geo-city"]
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+'&APPID=18db156f5ef1c6bdad1be1c5072fa282')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
     temp_c = int(temp_k - 273.15)
-    action_js = {"fulfillmentText": "A temperatura é de " + str(temp_c) + "graus",
-                     "fulfillmentMessages": [{"name": "projects/teste1-4a3c3/agent/intents/3b7a1380-cce5-4321-b98e-fbb6230e84c1",
-                                              "displayName": "Clima", "webhookState": "WEBHOOK_STATE_ENABLED", "priority": 5000,
-                                              "isFallback": False, "mlEnabled": True, "mlDisabled": False, "inputContextNames": [],
-                                              "events": [], "trainingPhrases": [{}], "action": "temperatura.clima",
-                                              "outputContexts": [{}], "resetContexts": False, "parameters": [{}],
-                                              "messages": [{"plataform": "google","text": "A temperatura é de " + str(temp_c) + "graus"}],
-                                              "defaultResponsePlatforms": ["Oi"], "rootFollowupIntentName": True,
-                                              "parentFollowupIntentName": "Clima", "followupIntentInfo": [{}]}],
-                     "source": "http://tempeclima.heroku.com"}
+    action_js = {"fulfillmentText": "A temperatura é de " + str(temp_c) + "graus"}
 
     return jsonify(action_js)  
     #return render_template('temperatura.html', temp=temp_c)
